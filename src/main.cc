6803aba83979c2bc35cc89ee1e84f3cdd3cebfa3
ErosM04 <eros.menin@gmail.com>
2025-01-10 21:52:15 +0100

    added power value reading from terminal in main

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,24 @@
 #include "../include/terminal.h"
+#include <iostream>
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    if(argc < 2){
+        std::cout << "Nessuna potenza fornita, il programma utilizzerà il valore di default 3.5kW\n\n";
+    }else{
+        double power;
+        
+        try{
+            power = std::stod(argv[1]);
+        }catch(const std::exception& e){
+            std::cout << "Errore: Il valore inserito come potenza risulta invalido" << std::endl;
+            return 1;
+        }
+        
+        // chiamata a terminal.run() a cui viene passata la potenza
+    }
+
     Terminal terminal;
     terminal.run();
     return 0;
