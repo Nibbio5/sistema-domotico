aad29397c808536156cf54be9084d023e28192cb
Fran4end <francyd04@gmail.com>
2025-01-12 12:39:58 +0100

    fixed some log bugs and add documentation on my class

--- include/log.h
+++ include/log.h
@@ -15,27 +15,56 @@
 #include "time.h"
 
 namespace report {
+
+/**
+ * @class message
+ * @brief A class that represents a log message.
+ *
+ * The message class represents a log message with a timestamp and a string.
+ */
 class message {
   private:
-    std::string messageString_;
-    Time messageTime_;
-    bool display_ ;
+    std::string messageString_; ///< The string of the message.
+    Time messageTime_; ///< The time of the message.
+    bool display_ ; ///< The display on terminal flag of the message.
 
   public:
+    /**
+     * @brief Default constructor for the message class.
+     */
     message(const Time &currentTime, const std::string &messageString, const bool &display = true);
 
+    /**
+     * @brief Gets the time of the message.
+     * @return The time of the message.
+     */
     Time messageTime(void) const {
         return messageTime_;
     }
 
+    /**
+     * @brief Gets the string of the message.
+     * @return The string of the message.
+     */
     std::string messageString(void) const {
         return messageString_;
     }
 
+    /**
+     * @brief Gets the display flag of the message.
+     * @return The display flag of the message.
+     */
     bool display(void) const {
         return display_;
     }
 
+    
+    /**
+     * @brief Overload of the << operator for the message class.
+     * @param out The output stream.
+     * @param msg The message to print.
+     * @return The output stream.
+     */
     friend std::ostream &operator<<(std::ostream &out, const message &msg) {
         if(!msg.display_) {
             return out << "[Command] " << msg.messageString_;;
@@ -43,22 +72,49 @@ class message {
         return  out << "[" << msg.messageTime_ << "] " << msg.messageString_;
     }
 };
+
+/**
+ * @class logs
+ * @brief A class that represents a log system.
+ *
+ * The logs class represents a log system that stores log messages and displays them.
+ */
 class logs {
   private:
     static std::string log_file_name_;
     static std::vector<message> logList_;
 
+    /**
+     * @brief sorts the log messages by time
+     * 
+     * @param newMessage 
+     */
     void insertionSort(message newMessage);
 
+    /**
+     * @brief Default constructor for the logs class.
+     */
     logs();
 
   public:
+    /**
+     * @brief Gets the instance of the logs class.
+     * @return The instance of the logs class.
+     */
     static logs &getInstance() {
         static logs instance;
         return instance;
     }
 
+    /**
+     * @brief Adds a log message to the log system.
+     * @param newMessage The new log message.
+     */
     void addLog(const report::message &newMessage);
+
+    /**
+     * @brief Displays the log messages.
+     */
     void displayLogs();
 };
 }
--- include/terminal.h
+++ include/terminal.h
@@ -23,18 +23,69 @@ class Terminal {
     DomoticsSystem domotics_system_;
     report::logs log_;
 
+    /**
+     * @brief Sets the current time in the system.
+     * @param arg The new time to set.
+     */
     void setTimeCommandPrompt(const std::string &arg);
-    Device *isDevice(const std::string &device_name);
-    void setDeviceCommandPrompt(const std::vector<std::string> &args, Device *device);
-    std::string showOneDevice(const Device *device, const bool &show_time = true);
-    void setCommandPrompt(const std::vector<std::string> &args);
-    void rmCommandPrompt(const std::string &arg);
-    void showCommandPrompt(const std::string &arg);
-    void resetCommandPrompt(const std::string &arg);
-    void setDeviceTimer(const std::string &device_name, const std::string &start, const std::string &stop);
-    void helpCommandPrompt();
 
-    size_t split(const std::string &txt, std::vector<std::string> &strs, char ch);
+    /**
+     * @brief Checks if a device is present in the system.
+     * @param arg The name of the device.
+     * @return The device if present, nullptr otherwise.
+     */
+    Device *isDevice(const std::string &device_name);
+
+    /**
+     * @brief Sets Device command prompt based on the arguments.
+     * @param args The arguments of the command.
+     */
+    void setDeviceCommandPrompt(const std::vector<std::string> &args, Device *device);
+
+    /**
+     * @brief Shows the status of a single device.
+     * @param device The device to show.
+     * @param show_time The flag to show the time at the start.
+     * @return The string of the device status.
+     */
+    std::string showOneDevice(const Device *device, const bool &show_time = true);
+
+    /**
+     * @brief Sets command prompt based on the arguments.
+     * @param args The arguments of the command.
+     */
+    void setCommandPrompt(const std::vector<std::string> &args);
+
+    /**
+     * @brief Removes the timer of a device.
+     * @param arg The name of the device.
+     */
+    void rmCommandPrompt(const std::string &arg);
+
+    /**
+     * @brief Shows the status of system devices or one device ({showOneDevice}).
+     * @param arg The arguments of the command.
+     */
+    void showCommandPrompt(const std::string &arg);
+
+    /**
+     * @brief Resets command prompt based on the arguments.
+     * @param arg The arguments of the command.
+     */
+    void resetCommandPrompt(const std::string &arg);
+
+    /**
+     * @brief Sets the timer of a device.
+     * @param device_name The name of the device.
+     * @param start The start time.
+     * @param stop The stop time.
+     */
+    void setDeviceTimer(const std::string &device_name, const std::string &start, const std::string &stop);
+
+    /**
+     * @brief Shows the help message.
+     */
+    void helpCommandPrompt();
 
   public:
     Terminal(double power_limit = 3.5);
@@ -43,7 +94,17 @@ class Terminal {
 };
 
 /**
- * @brief Helper function that rounds a given double value to the diresred precision
+ * @brief Helper function that splits a string into a vector of strings based on a delimiter.
+ *
+ * @param txt The string to split.
+ * @param strs The vector of strings to store the result.
+ * @param ch The delimiter character.
+ * @return size_t The size of the vector of strings.
+ */
+static size_t split(const std::string &txt, std::vector<std::string> &strs, char ch);
+
+/**
+ * @brief Helper function that rounds a given double value to the desired precision
  *
  * @param value the double value to round
  * @param precision the decimal precision
